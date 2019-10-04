ae61028df7cac47857f9bddf1cc98eb55d2d0259
ALUMNO05-J1401 <alumno05-j1401@TLSCPU1949.local>
2019-10-04 18:31:00 -0500

    Initial Commit

---
+++ CannaMed/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CannaMed
+//
+//  Created by ALUMNO05-J1401 on 10/4/19.
+//  Copyright © 2019 Toulouse-Lautrec. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
