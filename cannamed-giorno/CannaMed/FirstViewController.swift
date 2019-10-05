a28d4caece0b985f62ba90c52ad70fa7ecb067d4
Giorgio <jlqs97@gmail.com>
2019-10-04 21:33:26 -0500

    Hola

---
+++ cannamed-giorno/CannaMed/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CannaMed
+//
+//  Created by ALUMNO05-J1401 on 10/4/19.
+//  Copyright © 2019 Toulouse-Lautrec. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
